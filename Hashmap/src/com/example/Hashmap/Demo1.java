1bc55d8f8a5c602462e0a0958cbd902cd3689cd5
Soebk0122 <weisui122812@gmail.com>
2026-09-09 20:15:39 +0800

    更新

---
+++ Hashmap/src/com/example/Hashmap/Demo1.java
@@ -0,0 +1,20 @@
+package com.example.Hashmap;
+
+import java.util.HashMap;
+
+//两数之和
+public class Demo1 {
+    public int[] twoSum(int[] nums, int target){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int x=nums[i];
+            int y=target-x;
+            if (map.containsKey(y)){
+                return new int[]{map.get(y),i};
+            }else{
+                map.put(x,i);
+            }
+        }
+        return null;
+    }
+}
---
+++ jiqiao/src/com/example/jiqiao/Demo1.java
@@ -0,0 +1,13 @@
+package com.example.jiqiao;
+
+public class Demo1 {
+    public int singleNumber(int[] nums){
+        int ans=nums[0];
+        if (nums.length>1){
+            for (int i = 1; i < nums.length; i++) {
+                ans=ans^nums[i];
+            }
+        }
+        return ans;
+    }
+}
