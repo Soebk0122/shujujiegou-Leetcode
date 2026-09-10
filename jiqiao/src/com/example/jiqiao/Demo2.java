1ad712dd54171fc3d3e7bb526f4369075d2ec4ef
Soebk0122 <weisui122812@gmail.com>
2026-09-10 23:16:19 +0800

    更新

--- jiqiao/src/com/example/jiqiao/Demo2.java
+++ jiqiao/src/com/example/jiqiao/Demo2.java
@@ -16,3 +16,16 @@ public class Demo2 {
     }
 
 }
+//public int majorityElement(int[] nums) {
+//    int ans=0;
+//    int hp=0;
+//    for(int x:nums){
+//        if(hp==0){
+//            ans=x;
+//            hp=1;
+//        }else{
+//            hp+=x==ans?1:-1;
+//        }
+//    }
+//    return ans;
+//}
