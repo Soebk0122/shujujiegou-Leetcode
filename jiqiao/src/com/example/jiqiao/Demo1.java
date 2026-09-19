7b18520ed732a2645a54ee85810f01406cf8f98f
Soebk0122 <weisui122812@gmail.com>
2026-09-19 22:27:00 +0800

    更新

--- jiqiao/src/com/example/jiqiao/Demo1.java
+++ jiqiao/src/com/example/jiqiao/Demo1.java
@@ -49,3 +49,15 @@ public class Demo1 {
 /// /        return ans;
 /// /    }
 /// /}
+//}////136. 只出现一次的数字
+/// /public class Demo1 {
+/// /    public int singleNumber(int[] nums){
+/// /        int ans=nums[0];
+/// /        if (nums.length>1){
+/// /            for (int i = 1; i < nums.length; i++) {
+/// /                ans=ans^nums[i];
+/// /            }
+/// /        }
+/// /        return ans;
+/// /    }
+/// /}
